9c8f67e8db7105dbb029dfd4b0288cc4a726b91b
Johnny Soong <johnnysoong@hotmail.com>
2018-08-01 22:16:11 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 def key_for_min_value(hash)
   if hash.length == 0
-    nil
+    null
   min = hash.first[1]
   min_key = hash.first[0]
   hash.each do |key, value|
